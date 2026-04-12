3949938fed6b9c89d65dc3b7e45b0e8bbda4ef93
OUALID AAZAB <160775667+Oualidaazab@users.noreply.github.com>
2026-04-12 19:10:50 +0200

    Add message and encoding details as comments
    Added comments with message and encoded values.

--- challenge2.py
+++ challenge2.py
@@ -1,3 +1,11 @@
 from Crypto.Util.number import * 
 flag=11515195063862318899931685488813747395775516287289682636499965282714637259206269
 print(long_to_bytes(flag)) 
+""" 
+message: HELLO
+ascii bytes: [72, 69, 76, 76, 79]
+hex bytes: [0x48, 0x45, 0x4c, 0x4c, 0x4f]
+base-16: 0x48454c4c4f
+base-10: 310400273487 
+"""   
+# the flag is : crypto{3nc0d1n6_4ll_7h3_w4y_d0wn} 
